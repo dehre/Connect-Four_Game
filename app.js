49dbe75dcb5d59d6653af72a1ba6ee2b881b2566
Loris <loris4526@gmail.com>
2017-07-25 09:23:51 +0200

    Add 'click' eventListener on board. Taking list of classes for clicked element

--- app.js
+++ app.js
@@ -15,13 +15,35 @@ for(var i=0;i<rows;i++){
   // Add cells
   for(var j=0;j<columns;j++){
     var $newCell = $("<div></div>");
-    $newCell.addClass("board-cell row-"+i + " cell-"+j);
+    $newCell.addClass("board-cell row-"+i + " col-"+j);
     var $newInnerCircle = $("<div></div>");
     $newInnerCircle.addClass("pawn");
     $newInnerCircle.appendTo($newCell);
     $newCell.appendTo($gameContainer);
   }
 }
+
+// Add 'click' eventListener on entire board to fill the '.pawn'
+$gameContainer.click(function(e){
+  console.dir(e.target);
+  var completeClassList = [];
+  // e.target.classList.forEach(function(class){
+  //   completeClassList.push(class);
+  // });
+  // e.target.parentNode.classList.forEach(function(class){
+  //   completeClassList.push(class);
+  // });
+  for(var i=0;i<e.target.classList;i++){
+    completeClassList.push(e.target.classList[i]);
+  }
+  for(var i=0;i<e.target.parentNode.classList;i++){
+    completeClassList.push(e.target.parentNode.classList[i]);
+  }
+
+  console.log(completeClassList);
+  console.log("row clicked:");
+  console.log("column clicked:");
+});
 
 
 }); //end or '$document.ready()'
