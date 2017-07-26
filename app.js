c8995c0978736209c296e4065ed13b6565eb6e30
Loris <loris4526@gmail.com>
2017-07-26 08:38:20 +0200

    Modify 'takeClickedPawnPosition' to return jQuery representation of clicked pawn

--- app.js
+++ app.js
@@ -5,34 +5,38 @@ var columns = 7;
 var rows = 6;
 var lastPlayedPawn = {};
 
+
 // // DOM references
 var $gameContainer = $(".game-container");
+
+
 
 // // Helper functions
 
 // take row and column number of clicked pawn
 function takeClickedPawnPosition(pawnDOMElement){
-  // create empty object that contains current position of pawn
-  var pawnPositionObj = {};
-  // create list of classes related to the clicked element
-  var elementClassList = [];
-  // check if clicked on a pawn itself or on the board
+  // create empty object that contains current position of pawn, and pawn itself as jQuery object
+  var pawnObj = {};
+  // first check if clicked on a pawn itself or on the board
   if($(pawnDOMElement).hasClass("board-cell")){
-    elementClassList = $(pawnDOMElement).attr("class").split(" ");
+    pawnObj.jQueryElement = $(pawnDOMElement);
   } else {
-    elementClassList = $(pawnDOMElement).parent().attr("class").split(" ");
+    pawnObj.jQueryElement = $(pawnDOMElement).parent();
   }
+  // create list of classes related to the clicked element
+  var elementClassList = pawnObj.jQueryElement.attr("class").split(" ");
   // retrieve the clicked row and column clicked (as number)
   elementClassList.forEach(function(className){
     if(className.indexOf("row-")===0){
-      pawnPositionObj.row = className.substr(-1);
+      pawnObj.row = className.substr(-1);
     } else if(className.indexOf("col-")===0) {
-      pawnPositionObj.col = className.substr(-1);
+      pawnObj.col = className.substr(-1);
     }
   });
   // return object filled with current position of element in the board
-  return pawnPositionObj;
+  return pawnObj;
 }
+
 
 
 // // Create board programmatically
@@ -48,6 +52,8 @@ for(var i=rows-1;i>=0;i--){
     $newCell.appendTo($gameContainer);
   }
 }
+
+
 
 // // Add 'click' eventListener on entire board to fill the '.pawn'
 $gameContainer.click(function(e){
