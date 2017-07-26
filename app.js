822bced0872605f3590482fece4c8f2c9ee3a81f
Loris <loris4526@gmail.com>
2017-07-26 08:20:30 +0200

    Create 'takeClickedPawnPosition()' to retrieve as object the position of currently clicked pawn

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 $(document).ready(function(){
 
-// Settings - varible declarations
+// // Settings - varible declarations
 var columns = 7;
 var rows = 6;
-var clickedRow;
-var clickedColumn;
+var lastPlayedPawn = {};
 
-// DOM references
+// // DOM references
 var $gameContainer = $(".game-container");
 
+// // Helper functions
+
+// take row and column number of clicked pawn
+function takeClickedPawnPosition(pawnDOMElement){
+  // create empty object that contains current position of pawn
+  var pawnPositionObj = {};
+  // create list of classes related to the clicked element
+  var elementClassList = [];
+  // check if clicked on a pawn itself or on the board
+  if($(pawnDOMElement).hasClass("board-cell")){
+    elementClassList = $(pawnDOMElement).attr("class").split(" ");
+  } else {
+    elementClassList = $(pawnDOMElement).parent().attr("class").split(" ");
+  }
+  // retrieve the clicked row and column clicked (as number)
+  elementClassList.forEach(function(className){
+    if(className.indexOf("row-")===0){
+      pawnPositionObj.row = className.substr(-1);
+    } else if(className.indexOf("col-")===0) {
+      pawnPositionObj.col = className.substr(-1);
+    }
+  });
+  // return object filled with current position of element in the board
+  return pawnPositionObj;
+}
 
 
 // // Create board programmatically
@@ -27,24 +51,10 @@ for(var i=0;i<rows;i++){
 
 // // Add 'click' eventListener on entire board to fill the '.pawn'
 $gameContainer.click(function(e){
-  // bring list of classes related to the clicked element
-  var elementClassList = [];
-  if($(e.target).hasClass("board-cell")){
-    elementClassList = $(e.target).attr("class").split(" ");
-  } else {
-    elementClassList = $(e.target).parent().attr("class").split(" ");
-  }
-  console.log(elementClassList);
-  // retrieve the clicked row and column clicked (as number)
-  elementClassList.forEach(function(className){
-    if(className.indexOf("row-")===0){
-      clickedRow = className.substr(-1);
-    } else if(className.indexOf("col-")===0) {
-      clickedCol = className.substr(-1);
-    }
-  });
-  console.log("row clicked:", clickedRow);
-  console.log("column clicked:", clickedCol);
+
+  lastPlayedPawn = takeClickedPawnPosition(e.target);
+  console.log(lastPlayedPawn);
+
 });
 
 
